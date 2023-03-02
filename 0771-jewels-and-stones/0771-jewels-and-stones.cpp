93b3048a79ae9cdb579d3329c67e58b8a8868b69
Parthiv Sen <74867166+ParthivSen@users.noreply.github.com>
2023-03-02 18:32:44 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.29%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int ans = 0;
+        for(char i: jewels)
+            for(char j: stones)
+                if(i==j)
+                    ans++;
+        return ans;
+    }
+};
